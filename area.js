5cadecb6bf323ac359fe894e1e5fb7855dd28d16
Krishna M <krishna.mahadevaiah@gmail.com>
2022-10-14 09:34:37 +0530

    fix : positive values for area of triangle

--- area.js
+++ area.js
@@ -8,6 +8,7 @@ const findarea = document.querySelector("#check-button");
 const outputmsg = document.querySelector("#output-area")
 
 
+var flag;
 
 
 
@@ -18,19 +19,51 @@ function calcbintoh(a,b)
 }
 
 
-function areoft()
+// function areoft()
+// {
+//     // console.log("Hey");
+//     if(Number(sideinput[0].value)&&Number(sideinput[1].value))
+//     {
+//         const bh=calcbintoh(Number(sideinput[0].value),Number(sideinput[1].value));
+//         const areaoftriangle = (1/2)*bh;
+//         outputmsg.innerText = "Are of traingle is "+areaoftriangle;
+//     }
+//     else
+//     {
+//         outputmsg.innerText = "Please enter the inputs properly!"
+//     }
+    
+// }
+
+function checkinputvalues(side1, side2)
 {
-    // console.log("Hey");
-    if(Number(sideinput[0].value)&&Number(sideinput[1].value))
+    if(side1 < 0 || side2 < 0)
     {
-        const bh=calcbintoh(Number(sideinput[0].value),Number(sideinput[1].value));
-        const areaoftriangle = (1/2)*bh;
-        outputmsg.innerText = "Are of traingle is "+areaoftriangle;
+        flag = 0;
+        outputmsg.innerText = "Please enter positive values for the inputs!"
+    }
+    else if(Number(sideinput[0].value)&&Number(sideinput[1].value))
+    {
+        flag =1
     }
     else
     {
         outputmsg.innerText = "Please enter the inputs properly!"
     }
+}
+
+function areoft()
+{
+    // console.log("Hey");
+
+    checkinputvalues(Number(sideinput[0].value), Number(sideinput[1].value))
+    if(flag===1)
+    {
+        const bh=calcbintoh(Number(sideinput[0].value),Number(sideinput[1].value));
+        const areaoftriangle = (1/2)*bh;
+        outputmsg.innerText = "Are of traingle is "+areaoftriangle;
+    }
+
     
 }
 

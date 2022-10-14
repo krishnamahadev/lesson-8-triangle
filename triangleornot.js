523ee97fccf1448a8dd4f93eaa399d19256141c2
Krishna M <krishna.mahadevaiah@gmail.com>
2022-10-14 09:26:20 +0530

    fix : error handling for -ve angles

--- triangleornot.js
+++ triangleornot.js
@@ -4,7 +4,7 @@ const checkbutton = document.querySelector("#check-button");
 
 const dispoutput = document.querySelector("#display-here");
 
-
+var flag
 
 console.log(checkbutton);
 
@@ -18,6 +18,49 @@ function calculatesumofangles(angle1, angle2, angle3)
 }
 
 
+// function istriangle()
+// {
+//     console.log("hey")
+//     // calculatesumofangles(inputangle[0].value, inputangle[1].value,inputangle[2].value);
+//     sumoa = calculatesumofangles(Number(inputangle[0].value), Number(inputangle[1].value),Number(inputangle[2].value));
+//     console.log(sumoa);
+
+//     if(Number(inputangle[0].value) && Number(inputangle[1].value)&&Number(inputangle[2].value))
+//     {
+//         if(sumoa!= 180)
+//         {
+//             dispoutput.innerText="Entered angles do not form a triangle"
+//         }
+//         else
+//         {
+//             dispoutput.innerText="Entered angles do form a triangle! Kudos"
+//         }
+//     }
+//     else
+//     {
+//         dispoutput.innerText="please enter all the fields properly"
+//     }
+    
+
+// }
+
+function checkinputvalues(angle1, angle2, angle3)
+{
+    if (angle1 < 0 || angle2 <0 || angle3 <0)
+    {
+        dispoutput.innerText="please enter postive values for angles";
+        flag = 0;
+    }
+    else if (angle1 && angle2 && angle3)
+    {
+        flag = 1
+    }
+    else
+    {
+        dispoutput.innerText="please enter all the fields properly"
+    }
+}
+
 function istriangle()
 {
     console.log("hey")
@@ -25,7 +68,9 @@ function istriangle()
     sumoa = calculatesumofangles(Number(inputangle[0].value), Number(inputangle[1].value),Number(inputangle[2].value));
     console.log(sumoa);
 
-    if(Number(inputangle[0].value) && Number(inputangle[1].value)&&Number(inputangle[2].value))
+    checkinputvalues(Number(inputangle[0].value), Number(inputangle[1].value),Number(inputangle[2].value))
+
+    if(flag===1)
     {
         if(sumoa!= 180)
         {
@@ -36,13 +81,9 @@ function istriangle()
             dispoutput.innerText="Entered angles do form a triangle! Kudos"
         }
     }
-    else
-    {
-        dispoutput.innerText="please enter all the fields properly"
-    }
-    
 
 }
+
 
 
 checkbutton.addEventListener("click",istriangle);
